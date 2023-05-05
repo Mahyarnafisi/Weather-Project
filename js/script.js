741fc6a9c0e6f5857b38b55fa0f61736cab025e1
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-05 21:25:53 +0200

    adidng variable to start js file :)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+// container
+const appContainer = document.querySelector(".container .app");
+const weatherContainer = document.querySelector("weather-box");
+const detailContainer = document.querySelector(".weather-detail");
+const error404Container = document.querySelector(".not-found");
+// element
+const searchInput = document.querySelector(".search-box__input");
+// button
+const btnSearch = document.querySelector(".search-box__btn");
