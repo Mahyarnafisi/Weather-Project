33cd4801d530e913ad30b0070e5676d170c3974f
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-05 22:25:29 +0200

    succussfully fetch the api data to the project and print in console

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 "use strict";
 // container
-const appContainer = document.querySelector(".container .app");
-const weatherContainer = document.querySelector("weather-box");
-const detailContainer = document.querySelector(".weather-detail");
-const error404Container = document.querySelector(".not-found");
+const containerApp = document.querySelector(".container .app");
+const containerWeather = document.querySelector("weather-box");
+const containerDetail = document.querySelector(".weather-detail");
+const containerError404 = document.querySelector(".not-found");
 // element
-const searchInput = document.querySelector(".search-box__input");
+
 // button
 const btnSearch = document.querySelector(".search-box__btn");
+// API Keys
+
+// fetching from API//
+
+const fetching = btnSearch.addEventListener("click", () => {
+  const apiKeys = "cef6bfc4d64c3a8d9f6afcc10e72ae43";
+  const city = document.querySelector(".search-box__input").value;
+  console.log(`${city}`);
+  if (city === "") return;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKeys}`)
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.message === "city not found") {
+        containerError404.classList.add(".fade-in");
+        containerError404.style.display = "flex";
+        document.querySelector(".not-found .not-found__message ").innerHTML = "City not found, please try again";
+        return;
+      }
+      containerError404.style.display = "none";
+      containerError404.classList.remove(".fade-in");
+      console.log(json);
+    });
+});
